fce371d023e0a6c1f0776a1073a9aaf335c055b7
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2022-02-07 22:09:00 +0100

    Fix timer

--- sparkplug/src/timer.h
+++ sparkplug/src/timer.h
@@ -4,15 +4,18 @@
 
 struct Timer
 {
-  Timer(uint32_t interval_) : interval{interval_} {}
+  Timer(uint32_t interval_) : interval(interval_) {}
 
   bool elapsed(uint32_t currentMillis)
   {
-    bool elapsed = currentMillis - previousMillis >= interval;
-    if (elapsed) previousMillis = (currentMillis - previousMillis - interval);
-    return elapsed;
+    if (currentMillis - previousMillis > interval)
+    {
+      previousMillis = currentMillis - (currentMillis - previousMillis - interval);
+      return true;
+    }
+    return false;
   }
 
-  uint32_t previousMillis;
+  uint32_t previousMillis = 0;
   uint32_t interval;
 };
