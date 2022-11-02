1dc520cce15414f1ab38dce5197aab476527a389
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2022-11-02 19:17:23 +0100

    Implement blink counts

--- sparkplug/configs/default/presets.h
+++ sparkplug/configs/default/presets.h
@@ -35,7 +35,7 @@ const PROGMEM Preset presetsHighBeams[] =
 const PROGMEM Preset presetsBlinkersFrontLeft[] =
     {
         {.modeID = Parking, .intensity = 4},
-        {.modeID = BlinkL, .mode = PresetModes::Blink},
+        {.modeID = BlinkL, .mode = PresetModes::Blink, .blinkCount = 2},
         {.modeID = Hazards, .mode = PresetModes::Blink},
 
         {.modeID = Police, .mode = PresetModes::Blink, .fadeSpeedRising = 50, .fadeSpeedFalling = 100, .blinkInterval = 2400, .blinkPattern = 0b10100000101000001111000011110000},
@@ -44,7 +44,7 @@ const PROGMEM Preset presetsBlinkersFrontLeft[] =
 const PROGMEM Preset presetsBlinkersFrontRight[] =
     {
         {.modeID = Parking, .intensity = 4},
-        {.modeID = BlinkR, .mode = PresetModes::Blink},
+        {.modeID = BlinkR, .mode = PresetModes::Blink, .blinkCount = 2},
         {.modeID = Hazards, .mode = PresetModes::Blink},
 
         {.modeID = Police, .mode = PresetModes::Blink, .fadeSpeedRising = 50, .fadeSpeedFalling = 100, .blinkInterval = 2400, .blinkPattern = 0b00001010000010100000111100001111},
--- sparkplug/src/types/preset.h
+++ sparkplug/src/types/preset.h
@@ -13,4 +13,5 @@ struct Preset
   uint16_t fadeSpeedFalling = 200;
   int16_t blinkInterval = 600;
   uint32_t blinkPattern = 0b11111111111111110000000000000000;
+  int16_t blinkCount = -1; // Infinite
 };
