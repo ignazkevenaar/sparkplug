4ca7df9ddabe6a474175d77e5662c9441ee36209
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2023-03-21 08:09:10 +0100

    Implement build-time config

--- coilpack/vite.config.js
+++ coilpack/vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
+
+const env = loadEnv("mock", process.cwd(), "");
+const configName = env['VITE_CONFIG'];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: `./configs/${configName}/*`,
+          dest: './config/'
+        }
+      ]
+    })
+  ],
   server: {
     port: 3000,
     strictPort: true
