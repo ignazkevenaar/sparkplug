18abc27b934fb5e3882bb08c63649861392757f8
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2024-03-02 22:25:30 +0100

    Add Vite Basic SSL

--- coilpack/vite.config.js
+++ coilpack/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import vue from "@vitejs/plugin-vue";
 
@@ -17,6 +18,7 @@ export default defineConfig({
         },
       ],
     }),
+    basicSsl(),
   ],
   server: {
     port: 3000,
