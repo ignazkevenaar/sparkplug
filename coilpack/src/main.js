d729cf6b509c1a7edf39f1ba689377e95f8a46cc
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2024-03-15 18:44:05 +0100

    Add Google fonts to project

--- coilpack/src/main.js
+++ coilpack/src/main.js
@@ -1,6 +1,7 @@
 import "./styles/index.css";
 import "./styles/transitions.css";
 import "./styles/theme.scss";
+import "./styles/fonts.css";
 import * as mdijs from "./config/icons";
 import App from "./App.vue";
 import { createApp } from "vue";
