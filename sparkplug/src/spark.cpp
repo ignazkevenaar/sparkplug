eb8f72d44997825df988ef76b715c34bf89d5424
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2024-07-25 22:15:26 +0200

    WIP

--- sparkplug/src/spark.cpp
+++ sparkplug/src/spark.cpp
@@ -14,6 +14,7 @@ void sparkInitialize()
 {
     welcomeMessage();
 
+    setupChannels();
     setupWiFi();
     setupUpdateServer();
     setupWebServer();
--- sparkplug/src/types/channel.h
+++ sparkplug/src/types/channel.h
@@ -6,13 +6,13 @@ struct Channel
 {
     int presetCount = 0;
     const Preset *presets = NULL;
+    bool *presetsActive;
 
     // Fade.
     uint16_t transitionFrom = 0;
     uint16_t transitionTo = 0;
     uint16_t value = 0;
     int16_t activePresetIndex = -1;
-    int16_t previousPresetIndex = -1;
     uint16_t targetDelay = 0;
     uint32_t startTime = 0;
 
