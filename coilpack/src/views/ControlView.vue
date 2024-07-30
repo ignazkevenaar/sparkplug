2e1fcc4ee6e8d08d6fb52328b71829a5829211fd
Ignaz Kevenaar <ignazkevenaar@gmail.com>
2024-07-30 22:15:23 +0200

    Replace legacy model values with Vue 3 style bindings

--- coilpack/src/views/ControlView.vue
+++ coilpack/src/views/ControlView.vue
@@ -8,7 +8,7 @@ const controlsConfig = inject(controlsConfigKey)
 const controlModels = inject(controlModelsKey)
 
 defineEmits<{
-  input: [modeChange: ControlModel | unknown]
+  'update:modelValue': [modeChange: ControlModel | unknown]
 }>()
 </script>
 
@@ -16,15 +16,15 @@ defineEmits<{
   <div>
     <ControlPanel
       class="mx-auto flex flex-1 flex-col justify-between p-6 pb-2 lg:container md:flex md:flex-1 md:flex-row md:items-center md:justify-center md:px-8 md:pb-6 lg:px-12 lg:pb-12 xl:px-20 xl:pb-10"
-      :value="controlModels"
+      :modelValue="controlModels"
       :control-config="controlsConfig?.status"
-      @input="$emit('input', $event)"
+      @update:modelValue="$emit('update:modelValue', $event)"
     />
     <ControlPanel
       class="mx-auto flex flex-1 flex-col justify-between bg-controls-background p-6 lg:container md:flex md:flex-1 md:flex-row md:items-center md:justify-center md:px-8 md:pb-8 lg:px-12 lg:pb-12 xl:px-20 xl:pb-20"
-      :value="controlModels"
+      :modelValue="controlModels"
       :control-config="controlsConfig?.controls"
-      @input="$emit('input', $event)"
+      @update:modelValue="$emit('update:modelValue', $event)"
     />
   </div>
 </template>
